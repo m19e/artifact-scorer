9c110af349ffcc2e4b4a8393401d0dd3c7b88d88
m19e <nexus.me7290@gmail.com>
2022-05-17 04:49:07 +0900

    Only recognize once

--- src/pages/reader/index.tsx
+++ src/pages/reader/index.tsx
@@ -11,6 +11,49 @@ interface Artifact {
   paramValue: number
 }
 
+const reg = new RegExp("[\u{2460}-\u{2468}]", "u")
+
+const trimCircleFromNumber = (text: string): string => {
+  return Array.from(text)
+    .map((c) => {
+      if (c.match(reg)) {
+        return String(+(c.codePointAt(0)?.toString(16) ?? "0") - 2459)
+      }
+      return c
+    })
+    .join("")
+}
+
+const getArtifactType = () => {
+  //
+}
+
+const getArtifactData = (text: string): Artifact => {
+  const line = text.replace(/\s/g, "")
+  const [option, prm] = line.split("+")
+  const param = trimCircleFromNumber(prm)
+  const label = option + "+" + param
+  const isPercent = param.includes("%")
+  const paramType: Artifact["paramType"] = isPercent ? "percent" : "actual"
+  const paramValue = +param.split("%").join("")
+  return { label, option, param, paramType, paramValue }
+}
+
+const getArtifactScore = (datas: Artifact[]): number => {
+  return datas
+    .map(({ paramType, option, paramValue }) => {
+      const isPercent = paramType === "percent"
+      if (isPercent && (option.includes("攻") || option.includes("会心"))) {
+        if (option.includes("率")) {
+          return paramValue * 2
+        }
+        return paramValue
+      }
+      return 0
+    })
+    .reduce((sum, elem) => sum + elem, 0)
+}
+
 const App = () => {
   const [file, setFile] = useState<ImageLike>("")
   const [url, setUrl] = useState("")
@@ -23,58 +66,28 @@ const App = () => {
 
   const tryOcr = async () => {
     await worker.load()
-    await worker.loadLanguage("jpn+eng")
-
-    await worker.initialize("eng")
-    await worker.setParameters({
-      tessedit_char_whitelist:
-        // "会心率ダメージ攻撃力元素チャージ効率HP防御力熟知+.0123465789①②③④⑤⑥⑦⑧⑨%",
-        "+.0123465789%",
-    })
-    const {
-      data: { text: parameters },
-    } = await worker.recognize(file)
-    const fmtParams = parameters
-      .split("\n")
-      .filter((l) => !!l)
-      .map((line) => line.split("+").slice(-1).join(""))
+    await worker.loadLanguage("jpn")
 
     await worker.initialize("jpn")
     await worker.setParameters({
       tessedit_char_whitelist:
-        "会心率ダメージ攻撃力元素チャージ効率HP防御力熟知+.①②③④⑤⑥⑦⑧⑨%",
+        "会心率ダメージ攻撃力元素チャージ効率HP防御力熟知+.0①②③④⑤⑥⑦⑧⑨%",
     })
     const {
-      data: { text: options },
+      data: { text },
     } = await worker.recognize(file)
-    const fmtOpts = options
+
+    const datas: Artifact[] = text
       .split("\n")
-      .filter((l) => !!l)
-      .map((line) => line.split("+")[0])
-
-    const zipped: Artifact[] = fmtOpts.map((opt, i) => {
-      const option = opt.replace(/\s/g, "")
-      const param = fmtParams[i].replace(/\s/g, "")
-      const label = option + "+" + param
-
-      const isPercent = param.includes("%")
-      const paramType: Artifact["paramType"] = isPercent ? "percent" : "actual"
-      const paramValue = +param.split("%").join("")
-      if (isPercent && (option.includes("攻") || option.includes("会心"))) {
-        if (option.includes("率")) {
-          setScore((prev) => prev + paramValue * 2)
-        } else {
-          setScore((prev) => prev + paramValue)
-        }
-      }
-      return { label, option, param, paramType, paramValue }
-    })
-    setArtifacts(zipped)
+      .filter((l) => Boolean(l))
+      .map(getArtifactData)
+    const newScore = getArtifactScore(datas)
+    setArtifacts(datas)
+    setScore(newScore)
 
     await worker.terminate()
   }
 
-  // fileData 取得
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     if (!e.target.files) {
       return
@@ -90,15 +103,16 @@ const App = () => {
   }
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col gap-4 items-center p-8">
       <input type="file" onChange={handleChange} />
       {!!url && <img src={url} />}
-      <button className="button" onClick={handleClick}>
+      <button className="btn" onClick={handleClick}>
         Try OCR
       </button>
       <span className="whitespace-pre-wrap">
         {JSON.stringify(
           artifacts.map((a) => a.label),
+          //   artifacts,
           null,
           4
         )}
