5d689453c5c53f1657d3e7540edece8d13d3fa36
m19e <nexus.me7290@gmail.com>
2022-06-11 23:15:19 +0900

    Use border instead of shadow

--- src/components/atoms/Progress.tsx
+++ src/components/atoms/Progress.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const Progress: VFC<Props> = ({ label, progress }) => {
   return (
-    <div className="flex flex-col items-center py-2 px-4 rounded shadow bg-base-100">
+    <div className="flex flex-col items-center py-2 px-4 rounded border bg-base-100 border-base-300">
       <div className="h-6">
         {label !== "" ? (
           <span className="text-base-content">
