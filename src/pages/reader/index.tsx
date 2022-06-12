e8c0778c3b4fe4d56792dea4a13140f5babcc8b0
m19e <nexus.me7290@gmail.com>
2022-06-12 19:54:23 +0900

    Fix art-level badge spacing

--- src/pages/reader/index.tsx
+++ src/pages/reader/index.tsx
@@ -584,10 +584,10 @@ const App = () => {
             </div>
             <div className="flex flex-col gap-2 py-3 bg-orange-100">
               <div className="flex justify-between items-center px-4">
-                <div className="pr-1 h-5 text-white bg-slate-700 rounded">
-                  <div className="-mt-1.5">
-                    <span className="text-xl font-black text-white">+20</span>
-                  </div>
+                <div className="pr-1 pl-0.5 h-6 bg-slate-700 rounded">
+                  <span className="text-xl font-black leading-5 text-white">
+                    +20
+                  </span>
                 </div>
                 <TwitterShareButton url="" />
               </div>
