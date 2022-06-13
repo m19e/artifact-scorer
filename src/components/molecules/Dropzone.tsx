212b44bbc6be3b2321ba6393118d6a561d13388f
m19e <nexus.me7290@gmail.com>
2022-06-13 23:18:40 +0900

    Update dropzone message lang

--- src/components/molecules/Dropzone.tsx
+++ src/components/molecules/Dropzone.tsx
@@ -23,19 +23,38 @@ export const Dropzone: VFC<Props> = ({ onDrop }) => {
   return (
     <div
       {...getRootProps()}
-      className="grid place-items-center p-2 w-full max-w-sm h-32 card bg-base-200 rounded-box"
+      className="grid place-items-center p-2 w-full max-w-sm h-40 card bg-base-200 rounded-box"
     >
       <div className="flex flex-col justify-center items-center w-full h-full text-sm font-black text-center border-2 border-dashed sm:text-base border-base-300 text-base-content rounded-box">
         <input {...getInputProps()} />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+          />
+        </svg>
         {isDragActive ? (
-          <p>Drop the file here ...</p>
+          <p>ここに画像をドロップ…</p>
         ) : (
           <>
-            <p>{"Drag 'n' Drop image file here,"}</p>
-            <p>or click to select file</p>
+            <p>{"画像ファイルをドロップ"}</p>
+            <p className="text-sm text-black text-opacity-50">または</p>
+            <p>クリックして選択</p>
           </>
         )}
       </div>
     </div>
   )
 }
+
+// <p>Drop the file here ...</p>
+// <p>"Drag 'n' Drop image file here,"</p>
+// <p>or click to select file</p>
