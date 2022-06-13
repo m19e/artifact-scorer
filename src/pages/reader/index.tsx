8951ad2df4f274364681de85370adbeb456b2a06
m19e <nexus.me7290@gmail.com>
2022-06-13 20:14:58 +0900

    Set trimming help button

--- src/pages/reader/index.tsx
+++ src/pages/reader/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { Fragment, useState, useEffect, useCallback } from "react"
 import type { Dispatch, SetStateAction } from "react"
 import { createWorker } from "tesseract.js"
@@ -454,26 +455,72 @@ const App = () => {
               {url ? (
                 <div className="flex flex-col">
                   <div className="flex gap-4 items-center w-full">
-                    <div className="overflow-hidden flex-1 h-72 rounded-xl sm:w-72 bg-base-300">
+                    <div className="overflow-hidden relative flex-1 h-80 rounded-xl sm:w-80 bg-base-300">
+                      <div className="absolute top-1 left-1 z-50 h-6 rounded-full bg-base-100 dropdown">
+                        <label
+                          tabIndex={0}
+                          className="btn btn-circle btn-ghost btn-xs text-info"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-5 h-5"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </label>
+                        <div
+                          tabIndex={0}
+                          className="w-72 shadow-xl card dropdown-content bg-base-100"
+                        >
+                          <div className="p-4 card-body">
+                            <h2 className="text-base card-title">
+                              サブステータスを範囲選択してください
+                            </h2>
+                            <p className="text-xs">トリミング例:</p>
+                            <figure>
+                              <Image
+                                width={300}
+                                height={160}
+                                src="/trimming-example.png"
+                                alt="トリミング例"
+                              />
+                            </figure>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex absolute top-1 right-1 z-50 justify-center items-center w-9 h-9 bg-base-100 rounded-box">
+                        <button
+                          className="btn btn-sm btn-square"
+                          onClick={() => setUrl("")}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-6 h-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </button>
+                      </div>
                       <RectCropper url={url} onCrop={setRectangle} />
                     </div>
-                    <div className="btn" onClick={() => setUrl("")}>
+                    {/* <div className="btn" onClick={() => setUrl("")}>
                       delete
-                    </div>
+                    </div> */}
                   </div>
-                  {/* <div className="h-0 divider"></div>
-                  <div className="inline-flex gap-4 items-center">
-                    <div className="flex-1">
-                      <Progress label={textOcr} progress={progress} />
-                    </div>
-                    <button
-                      className="btn"
-                      disabled={!url}
-                      onClick={handleClick}
-                    >
-                      recognize
-                    </button>
-                  </div> */}
                 </div>
               ) : (
                 <Dropzone onDrop={handleDrop} />
