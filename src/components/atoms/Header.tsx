c1c91dbbfbff69497d807fa9adaf1a26f4ce5b0a
m19e <nexus.me7290@gmail.com>
2022-07-03 15:16:06 +0900

    Reset header font

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -3,9 +3,7 @@ export const Header = () => {
     <div className="md:max-w-md navbar bg-neutral text-neutral-content md:rounded-b-box">
       <div className="navbar-start"></div>
       <div className="navbar-center">
-        <a className="text-2xl tracking-wider normal-case font-renner btn btn-ghost">
-          #ArtifactScorer
-        </a>
+        <a className="text-2xl normal-case btn btn-ghost">#ArtifactScorer</a>
       </div>
       <div className="navbar-end"></div>
     </div>
