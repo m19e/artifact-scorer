0b38915824f14baddd413543ccd50a5ec0adeea2
m19e <nexus.me7290@gmail.com>
2022-07-24 22:51:19 +0900

    Toggle sort mode on/off

--- src/components/molecules/StoredArtifactList.tsx
+++ src/components/molecules/StoredArtifactList.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react"
+import { useState, Fragment } from "react"
 import type { FC } from "react"
 
 import type {
@@ -7,6 +7,7 @@ import type {
   CalcModeData,
   SubStatusBuildMap,
 } from "@/types/Scorer"
+import { ArtifactDropdownList } from "@/components/molecules/ArtifactDropdownList"
 import { EditModal } from "@/components/molecules/ArtifactEditModal"
 import { RemoveModal } from "@/components/atoms/ArtifactRemoveModal"
 import { ArtifactDropdown } from "@/components/atoms/ArtifactDropdown"
@@ -24,6 +25,8 @@ export const StoredArtifactList: FC<Props> = ({
   custom,
   onUpdate,
 }) => {
+  const [isSort, setIsSort] = useState(false)
+
   const handleRemove = (targetId: string) => {
     onUpdate((prev) => prev.filter((a) => a.id !== targetId))
   }
@@ -32,23 +35,41 @@ export const StoredArtifactList: FC<Props> = ({
   }
 
   return (
-    <>
-      <div className="grid grid-cols-5 gap-x-2.5 gap-y-1.5 sm:grid-cols-6">
-        {artifacts.map((art) => (
-          <ArtifactDropdown
-            key={"dropdown-" + art.id}
-            artifact={art}
-            calcMode={calcMode}
-            custom={custom}
-          />
-        ))}
-      </div>
+    <div className="flex flex-col gap-2 items-center">
+      <label className="pr-2 pl-3.5 w-full h-8 font-bold cursor-pointer label rounded-box bg-base-100">
+        <span className="label-text">並べ替え</span>
+        <input
+          type="checkbox"
+          className="checkbox"
+          checked={isSort}
+          onChange={(e) => setIsSort(e.target.checked)}
+        />
+      </label>
+      {isSort ? (
+        <ArtifactDropdownList
+          artifacts={artifacts}
+          calcMode={calcMode}
+          custom={custom}
+          onUpdate={onUpdate}
+        />
+      ) : (
+        <div className="grid grid-cols-5 gap-x-2.5 gap-y-1.5 sm:grid-cols-6">
+          {artifacts.map((art) => (
+            <ArtifactDropdown
+              key={"dropdown-" + art.id}
+              artifact={art}
+              calcMode={calcMode}
+              custom={custom}
+            />
+          ))}
+        </div>
+      )}
       {artifacts.map((art) => (
         <Fragment key={"modals-" + art.id}>
           <RemoveModal id={art.id} onRemove={handleRemove} />
           <EditModal artifact={art} onEdit={handleEdit} />
         </Fragment>
       ))}
-    </>
+    </div>
   )
 }
