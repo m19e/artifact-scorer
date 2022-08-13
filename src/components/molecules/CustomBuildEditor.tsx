31d1505f41e906a52c8e65c0ae68092e09abb78c
m19e <nexus.me7290@gmail.com>
2022-08-13 16:53:32 +0900

    Fix custom-build rate color for theme fit

--- src/components/molecules/CustomBuildEditor.tsx
+++ src/components/molecules/CustomBuildEditor.tsx
@@ -13,27 +13,30 @@ export const CustomBuildEditor: FC<Props> = ({
   if (calcMode.id !== "CUSTOM") return null
 
   return (
-    <div className="grid grid-cols-1 gap-2 p-4 border border-opacity-20 sm:grid-cols-2 bg-base-100 border-base-content rounded-box">
+    <div className="grid grid-cols-1 gap-y-2 p-4 border border-opacity-20 sm:grid-cols-2 bg-base-100 border-base-content rounded-box">
       {Object.values(custom).map((sub) => (
         <div key={sub.id} className="flex justify-between">
           <span className="font-normal">{sub.name}</span>
-          <input
-            type="number"
-            inputMode="decimal"
-            className="px-0 w-12 h-5 text-base font-black leading-3 text-slate-700 input input-xs"
-            min={0}
-            max={2}
-            step={0.1}
-            value={sub.value}
-            onChange={(e) => {
-              const value = e.currentTarget.valueAsNumber
-              setCustom((prev) => {
-                const newSub = { ...prev[sub.id], value }
-                const newCustom = { ...prev, [sub.id]: newSub }
-                return newCustom
-              })
-            }}
-          />
+          <div>
+            <span className="-mr-0.5">×</span>
+            <input
+              type="number"
+              inputMode="decimal"
+              className="pl-0.5 w-12 h-5 text-base font-black leading-3 input input-xs"
+              min={0}
+              max={2}
+              step={0.1}
+              value={sub.value}
+              onChange={(e) => {
+                const value = e.currentTarget.valueAsNumber
+                setCustom((prev) => {
+                  const newSub = { ...prev[sub.id], value }
+                  const newCustom = { ...prev, [sub.id]: newSub }
+                  return newCustom
+                })
+              }}
+            />
+          </div>
         </div>
       ))}
     </div>
