300c3cb0f40b5de412e2b18cce87b065d0efa18d
m19e <nexus.me7290@gmail.com>
2022-08-14 19:42:59 +0900

    Replace calc-mode-select by generics

--- src/components/atoms/Select/CalcMode.tsx
+++ src/components/atoms/Select/CalcMode.tsx
@@ -3,11 +3,26 @@ import type { FC } from "react"
 import type { CalcModeID } from "@/types/Scorer"
 import { CalcModeList } from "@/consts/Scorer"
 
+import { SelectInput } from "@/components/molecules/SelectInput"
+
+const CALCMODE_LIST = CalcModeList.map(({ label, id }) => ({
+  label,
+  value: id,
+}))
+
 interface Props {
   onSelect: (id: CalcModeID) => void
 }
 
 export const CalcModeSelect: FC<Props> = ({ onSelect }) => {
+  return (
+    <SelectInput
+      className="w-full select select-bordered rounded-box"
+      items={CALCMODE_LIST}
+      onSelect={onSelect}
+      defaultValue={"CRIT"}
+    />
+  )
   return (
     <select
       className="w-full select select-bordered rounded-box"
