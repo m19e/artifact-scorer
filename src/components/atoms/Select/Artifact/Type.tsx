2af61057e7eca8e876fd54888b199a4f7d145fb6
m19e <nexus.me7290@gmail.com>
2022-08-16 17:43:38 +0900

    Replace art-type-select by generics

--- src/components/atoms/Select/Artifact/Type.tsx
+++ src/components/atoms/Select/Artifact/Type.tsx
@@ -3,23 +3,25 @@ import type { FC } from "react"
 import type { ArtifactTypeID } from "@/types/Scorer"
 import { ArtifactTypeList } from "@/consts/Scorer"
 
+import { SelectInput } from "@/components/molecules/SelectInput"
+
 interface Props {
   defaultValue: ArtifactTypeID
   onSelect: (id: ArtifactTypeID) => void
 }
 
+const ARTTYPE_LIST = ArtifactTypeList.map(({ id, name }) => ({
+  label: name,
+  value: id,
+}))
+
 export const ArtifactTypeSelect: FC<Props> = ({ defaultValue, onSelect }) => {
   return (
-    <select
+    <SelectInput
       className="text-white text-opacity-90 artifact-select-sm"
+      items={ARTTYPE_LIST}
+      onSelect={onSelect}
       defaultValue={defaultValue}
-      onChange={(e) => onSelect(e.currentTarget.value as ArtifactTypeID)}
-    >
-      {ArtifactTypeList.map((a) => (
-        <option key={a.name} value={a.id}>
-          {a.name}
-        </option>
-      ))}
-    </select>
+    />
   )
 }
